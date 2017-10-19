988a851bcb983ab1c0eda1856b32104a72f0846d
Nikhil Bhaskar <nikhil38@gmail.com>
2017-10-19 17:13:18 +0400

    hub ui

--- src/layouts/home/Home.js
+++ src/layouts/home/Home.js
@@ -3,8 +3,10 @@ const Web3 = require("web3");
 import HubJson from '../../../../daohaus-contracts/build/contracts/Hub.json'
 const truffleContract = require("truffle-contract");
 const Hub = truffleContract(HubJson);
-window.web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545')); 
+window.web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
 Hub.setProvider(window.web3.currentProvider)
+
+import MembersList from '../../components/MembersList';
 
 // const Promise = require("bluebird");
 // MetaCoin.setProvider(web3.currentProvider);
@@ -13,20 +15,29 @@ Hub.setProvider(window.web3.currentProvider)
 
 
 class Home extends Component {
-  
+
   render() {
     let hub
-    Hub.deployed().then(_hub => {
-      hub = _hub
-      console.log(_hub)
-    })
+    Hub.deployed()
+      .then(_hub => {
+        hub = _hub;
+        return _hub;
+      })
+      .then(() => {
+        console.log(Hub.at(hub.address))
+        return hub.members.call();
+      })
+      .then(members => {
+        console.log(members);
+      })
     console.log('hub', hub )
     return(
       <main className="container">
         <div className="pure-g">
           <div className="pure-u-1-1">
-            <h1>Daohaus</h1>
-            {JSON.stringify(Hub)}
+            <h1>Hub</h1>
+            <h3>Members List</h3>
+            <MembersList/>
           </div>
         </div>
       </main>

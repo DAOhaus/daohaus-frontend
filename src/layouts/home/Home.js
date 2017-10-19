e971b73090d6e1bf15c09c3c0535a9af82366bc0
John D. Storey <johndangerstorey@gmail.com>
2017-10-19 17:37:19 +0400

    adds hub page

--- src/layouts/home/Home.js
+++ src/layouts/home/Home.js
@@ -1,44 +1,15 @@
 import React, { Component } from 'react'
-const Web3 = require("web3");
-import HubJson from '../../../../daohaus-contracts/build/contracts/Hub.json'
-const truffleContract = require("truffle-contract");
-const Hub = truffleContract(HubJson);
-window.web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
-Hub.setProvider(window.web3.currentProvider)
-
-import MembersList from '../../components/MembersList';
-
-// const Promise = require("bluebird");
-// MetaCoin.setProvider(web3.currentProvider);
-// Promise.promisifyAll(web3.eth, { suffix: "Promise" });
-// Promise.promisifyAll(web3.version, { suffix: "Promise" });
-
+import { Link } from 'react-router-dom'
 
 class Home extends Component {
 
   render() {
-    let hub
-    Hub.deployed()
-      .then(_hub => {
-        hub = _hub;
-        return _hub;
-      })
-      .then(() => {
-        console.log(Hub.at(hub.address))
-        return hub.members.call();
-      })
-      .then(members => {
-        console.log(members);
-      })
-    console.log('hub', hub )
     return(
       <main className="container">
         <div className="pure-g">
           <div className="pure-u-1-1">
-            <h1>Hub</h1>
-            <h3>Members List</h3>
-            <MembersList/>
-            {/*JSON.stringify(Hub)*/}
+            <h1>Daohaus</h1>
+            <Link to="/hub/asdf">Hub</Link>
           </div>
         </div>
       </main>
